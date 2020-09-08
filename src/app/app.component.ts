e545d4591592489196ec9a5276c75f6ccc0abc31
BerkSKP <berk99@live.dk>
2020-09-08 14:18:21 +0200

    all again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import {HttpClientModule, HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit():void{
+  }
+  movies = new FormGroup({
+    film: new FormControl('')
+  });
+  onSubmit() {
+    console.log(this.movies.value);
+    this.test1= this.movies.get('film').value;
+  }
+  test1:string=" ";
   title = 'biograf';
+  url = "";
+  selectFile(event) {
+    if(event.target.files) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0])
+      reader.onload = (event:any) => {
+        this.url = event.target.result
+      }
+    }
+  }
 }
